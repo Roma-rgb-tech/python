d7db600d3d38fee4ca8e254948b24c7d5ecb6abd
Chernyshev Roman <triplets.rc@gmail.com>
2026-01-30 10:10:36 +0200

    Последовательности: Сравнение типов

---
+++ basics/11.py
@@ -0,0 +1,8 @@
+# Последовательности: Сравнение типов
+#      Изменения    Порядок.   Одинаковые элементы
+# list   v            v              v
+# tuple  x            v              v
+# set    v            x              x
+# range  x            v              x
+# dict   v            x              x
+# str    x            v              v
